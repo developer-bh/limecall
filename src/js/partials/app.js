8de0093fb0639acdb861f40e9474231a855af9a9
bohrytsenko <hrytsenkobohdan@gmail.com>
2020-03-11 15:57:12 +0100

    last changes

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,5 +1,156 @@
 $( document ).ready(function() {
 
+    // Tooltip
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+
+    // Pricing tooltop
+
+    $(".hover-tooltip").mouseenter(function() {
+        $(this).find('.tooltip').show();
+    }).mouseleave(function() {
+        $(this).find('.tooltip').hide();
+    });
+
+    // All features fixed bar
+
+    var content = $('.af_list');
+    if (content.length) {
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+            var offset = content.offset();
+            var height = content.height();
+            if (offset.top <= scroll && scroll <= offset.top + height) {
+                $('.af_bar').addClass("fixed");
+            } else {
+                $('.af_bar').removeClass('fixed');
+            }
+        });
+    }
+
+    // Cache selectors
+
+    var lastId,
+        topMenu = $("#mainNav"),
+        topMenuHeight = topMenu.outerHeight()+15,
+        menuItems = topMenu.find("a"),
+        scrollItems = menuItems.map(function(){
+            var item = $($(this).attr("href"));
+            if (item.length) { return item; }
+        });
+
+    menuItems.click(function(e){
+        var href = $(this).attr("href"),
+            offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop + 40
+        }, 900);
+        e.preventDefault();
+    });
+
+    $(window).scroll(function(){
+        var fromTop = $(this).scrollTop()+topMenuHeight;
+
+        var cur = scrollItems.map(function(){
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        cur = cur[cur.length-1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            menuItems
+                .parent().removeClass("active")
+                .end().filter("[href='#"+id+"']").parent().addClass("active");
+        }
+    });
+
+    // Guides add class scroll
+
+    $(document).on("scroll", onScroll);
+
+    $('.guides__bar a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
+    function onScroll(event){
+        var scrollPos = $(document).scrollTop();
+        $('.guides__bar a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('.guides__bar a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
+    }
+
+    // Templates filter show/hide
+
+    $('.templates_filters li').on('click', function (e) {
+        $(this).toggleClass('active');
+        $('.temp_item').hide();
+        $('.templates_filters li.active').each(function() {
+            var cat= $(this).data('categoryType');
+            $('.temp_item[data-category-type="'+cat+'"]').show();
+        });
+        if ($('.templates_filters li.active').length < 1) {
+            $('.temp_item').show();
+        }
+    });
+
+    // Progress
+
+    $(document).on('ready', function() {
+        var winHeight = $(window).height(),
+            docHeight = $(document).height(),
+            progressBar = $('progress'),
+            max, value;
+
+        /* Set the max scrollable area */
+        max = docHeight - winHeight;
+        progressBar.attr('max', max);
+
+        $(document).on('scroll', function(){
+            value = $(window).scrollTop();
+            progressBar.attr('value', value);
+        });
+    });
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 500) {
+            $(".sticky_header").show();
+        } else {
+            $(".sticky_header").hide();
+        }
+    });
+
+    // Pricing
+
     $('.pricing_item__btn .btn_green').on('click', function () {
         $('.popup-button .material-icons').click();
     });
@@ -25,24 +176,41 @@ $( document ).ready(function() {
 
     // Hover nav
 
-    $('.menu-item-has-children').mouseenter(function() {
-        $(this).addClass('active');
-        $(this).children('.sub-menu').stop().fadeIn()
-    }).mouseleave(function() {
-        $(this).removeClass('active');
-        $(this).children('.sub-menu').stop().fadeOut()
-    });
+    if($(window).innerWidth() <= 767) {
+        $('.header_mobile .header_nav .menu-item-has-children').on('click', function () {
+            $(this).toggleClass('active');
+        });
+    } else {
+        $('.menu-item-has-children').mouseenter(function() {
+            $(this).addClass('active');
+            $(this).children('.sub-menu').stop().show()
+        }).mouseleave(function() {
+            $(this).removeClass('active');
+            $(this).children('.sub-menu').stop().hide()
+        });
+    }
 
     // Mobile nav
 
     $('.header_wrap__icon').on('click', function () {
-       $('.header_mobile').toggleClass('slide');
-       $('html').toggleClass('over');
-       $('.header_wrap__left').toggleClass('white');
-       $('.header_wrap__icon').toggleClass('white');
+       $('.header_mobile').fadeIn();
+       $('html').addClass('over');
+    });
+
+    $('.header_mobile__top .close').on('click', function () {
+        $('.header_mobile').fadeOut();
+        $('html').removeClass('over');
     });
 
     // Scroll to element
+
+    $(".pricing_all").on('click', function(e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: ($(target).offset().top)
+        }, 800);
+    });
 
     $(".how_header__text a").on('click', function(e) {
         e.preventDefault();
@@ -50,6 +218,14 @@ $( document ).ready(function() {
         $('html, body').animate({
             scrollTop: ($(target).offset().top)
         }, 800);
+    });
+
+    $(".glossary_list__letters .box a").on('click', function(e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: ($(target).offset().top)
+        }, 1000);
     });
 
     // Contact options
@@ -96,6 +272,12 @@ $( document ).ready(function() {
         all.not(tgts).hide();
         tgts.show();
     });
+
+    // Refunds accordion
+
+    $('.accordion .btn').on('click', function () {
+        $(this).toggleClass('opened');
+    });
 });
 
 // AOS
@@ -103,6 +285,30 @@ $( document ).ready(function() {
 AOS.init({
     once: false,
 });
+
+$( document ).ready(function() {
+
+    // Intercom on mobile
+
+    // var isOnMobile = function() {
+    //     if(window.innerWidth <= 800) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // };
+    //
+    // console.log(isOnMobile());
+    //
+    // window.intercomSettings = {
+    //     app_id: 'tt29cfh9',
+    //     email: 'sathish@limecall.com',
+    //     hide_default_launcher: isOnMobile()
+    // };
+
+});
+
+
 
 
 
